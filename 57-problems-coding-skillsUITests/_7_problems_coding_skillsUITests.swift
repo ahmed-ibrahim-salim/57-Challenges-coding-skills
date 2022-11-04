89863d70a440d1f2124b39c9b8872c20748641b6
Ahmed ibrahim <48734681+ahmed-ibrahim-salim@users.noreply.github.com>
2022-11-04 08:52:20 +0200

    initial project

---
+++ 57-problems-coding-skillsUITests/_7_problems_coding_skillsUITests.swift
@@ -0,0 +1,41 @@
+//
+//  _7_problems_coding_skillsUITests.swift
+//  57-problems-coding-skillsUITests
+//
+//  Created by Ahmad medo on 04/11/2022.
+//
+
+import XCTest
+
+final class _7_problems_coding_skillsUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
