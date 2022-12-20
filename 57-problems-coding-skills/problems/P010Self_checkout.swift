7007f1b4ad9bcee9694704689cf7e15a238844de
Ahmed ibrahim <48734681+ahmed-ibrahim-salim@users.noreply.github.com>
2022-12-20 13:22:19 +0200

    started p010

---
+++ 57-problems-coding-skills/problems/P010Self_checkout.swift
@@ -0,0 +1,32 @@
+//
+//  P010Self_checkout.swift
+//  57-problems-coding-skills
+//
+//  Created by magdy khalifa on 20/12/2022.
+//
+
+import Foundation
+
+class CashierCalcutaltor{
+    
+    
+}
+
+func startCashierCalculator() throws{
+        
+    print("how many items",terminator: "")
+    
+    let numOfItems = readLine()
+    
+    let numAsInt = try Helper().checkInputAsInt(input: numOfItems)
+    
+    for item in 0..<numAsInt{
+        
+    }
+    
+    
+    let cashierCalculator = CashierCalcutaltor()
+    
+    
+    
+}
---
+++ 57-problems-coding-skillsTests/SelfCheckoutTests.swift
@@ -0,0 +1,20 @@
+//
+//  SelfCheckoutTests.swift
+//  57-problems-coding-skillsTests
+//
+//  Created by magdy khalifa on 20/12/2022.
+//
+
+import XCTest
+
+@testable import _7_problems_coding_skills
+
+final class SelfCheckoutTests: XCTestCase {
+
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+
+
+
+}
